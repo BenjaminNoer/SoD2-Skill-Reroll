7d461ebb9f0ad1ce9b8d49ad11586a12f8fa1827
Benjamin Noer <60319493+BenjoKazooie@users.noreply.github.com>
2021-07-09 14:56:32 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace SoD2_Reroll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbSurvivor1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbSurvivor2 = new System.Windows.Forms.ComboBox();
+            this.cbSurvivor3 = new System.Windows.Forms.ComboBox();
+            this.nudWait = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbResolution = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWait)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbSurvivor1
+            // 
+            this.cbSurvivor1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbSurvivor1.FormattingEnabled = true;
+            this.cbSurvivor1.Location = new System.Drawing.Point(137, 6);
+            this.cbSurvivor1.Name = "cbSurvivor1";
+            this.cbSurvivor1.Size = new System.Drawing.Size(284, 37);
+            this.cbSurvivor1.TabIndex = 0;
+            this.cbSurvivor1.SelectedIndexChanged += new System.EventHandler(this.cbSurvivor1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Survivor 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Survivor 3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Survivor 2";
+            // 
+            // cbSurvivor2
+            // 
+            this.cbSurvivor2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbSurvivor2.FormattingEnabled = true;
+            this.cbSurvivor2.Location = new System.Drawing.Point(137, 49);
+            this.cbSurvivor2.Name = "cbSurvivor2";
+            this.cbSurvivor2.Size = new System.Drawing.Size(284, 37);
+            this.cbSurvivor2.TabIndex = 4;
+            this.cbSurvivor2.SelectedIndexChanged += new System.EventHandler(this.cbSurvivor2_SelectedIndexChanged);
+            // 
+            // cbSurvivor3
+            // 
+            this.cbSurvivor3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbSurvivor3.FormattingEnabled = true;
+            this.cbSurvivor3.Location = new System.Drawing.Point(137, 92);
+            this.cbSurvivor3.Name = "cbSurvivor3";
+            this.cbSurvivor3.Size = new System.Drawing.Size(284, 37);
+            this.cbSurvivor3.TabIndex = 5;
+            this.cbSurvivor3.SelectedIndexChanged += new System.EventHandler(this.cbSurvivor3_SelectedIndexChanged);
+            // 
+            // nudWait
+            // 
+            this.nudWait.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudWait.Location = new System.Drawing.Point(356, 135);
+            this.nudWait.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.nudWait.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudWait.Name = "nudWait";
+            this.nudWait.Size = new System.Drawing.Size(65, 35);
+            this.nudWait.TabIndex = 6;
+            this.nudWait.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudWait.ValueChanged += new System.EventHandler(this.nudWait_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(277, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Wait after start (seconds)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(128, 29);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Resolution";
+            // 
+            // cbResolution
+            // 
+            this.cbResolution.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbResolution.FormattingEnabled = true;
+            this.cbResolution.Location = new System.Drawing.Point(146, 176);
+            this.cbResolution.Name = "cbResolution";
+            this.cbResolution.Size = new System.Drawing.Size(275, 37);
+            this.cbResolution.TabIndex = 9;
+            this.cbResolution.SelectedIndexChanged += new System.EventHandler(this.cbResolution_SelectedIndexChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(222, 219);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(199, 65);
+            this.btnStart.TabIndex = 10;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(17, 219);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(199, 65);
+            this.btnStop.TabIndex = 11;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 300);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.cbResolution);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nudWait);
+            this.Controls.Add(this.cbSurvivor3);
+            this.Controls.Add(this.cbSurvivor2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbSurvivor1);
+            this.Name = "Form1";
+            this.Text = "SoD2 Reroll";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudWait)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbSurvivor1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbSurvivor2;
+        private System.Windows.Forms.ComboBox cbSurvivor3;
+        private System.Windows.Forms.NumericUpDown nudWait;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbResolution;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using Patagames.Ocr;
+using System.Timers;
+using WindowsInput.Native;
+using WindowsInput;
+using System.Text.RegularExpressions;
+
+namespace SoD2_Reroll
+{
+    public partial class Form1 : Form
+    {
+        private System.Timers.Timer rerollTimer;
+        private readonly InputSimulator sim = new InputSimulator();
+        private string path;
+        private bool reroll = false;
+        private short wait = 10000, interval = 100, survivor = 1;
+        private Size resolution = new Size(1920, 1080);
+
+        private readonly static string[] skills = 
+        { 
+            "Acting", "Administration", "Animal Facts", "Bartending", "Business", "Combat Medicine", "Comedy", "Demolitions", 
+            "Design", "Driving", "Excuses", "Farting Around", "Firearms Maintenance", "Fishing", "Foraging", "Fortifications", 
+            "Geek Trivia", "Gut Packing", "Hacking", "Hairdressing", "Hygiene", "Ikebana", "Infrastructure", "Law", "Lichenology", 
+            "Literature", "Logistics", "Making Coffee", "Mobile Operations", "Movie Trivia", "Music", "Painting", "People Skills", 
+            "Pinball", "Poker Face", "Police Procedure", "Political Science", "Recycling", "Scrum Certification", "Self-Promotion", 
+            "Sewing", "Sexting", "Shopping", "Shopping", "Sleep Psychology", "Sports Trivia", "Soundproofing", "Tattoos", 
+            "TV Trivia", "Chemistry", "Computers", "Cooking", "Craftsmanship", "Gardening", "Mechanics", "Medicine", "Utilities" 
+        };
+
+        private string[] active = { "", "", "" };
+
+        public Form1() { InitializeComponent(); }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbSurvivor1.Items.AddRange(skills);
+            cbSurvivor2.Items.AddRange(skills);
+            cbSurvivor3.Items.AddRange(skills);
+
+            cbResolution.Items.AddRange(new string[] { "1280x720", "1360x720", "1366x720", "1600x900", "1920x1080", "2560x1440" });
+            cbResolution.SelectedIndex = 4;
+
+            rerollTimer = new System.Timers.Timer(wait);
+            rerollTimer.Elapsed += RerollTimer_Elapsed;
+
+            path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\SoD2Screenshot.jpg";
+
+            Stop();
+        }
+
+        private void RerollTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            rerollTimer.Enabled = false;
+            rerollTimer.Interval = interval;
+
+            int left = 0;
+            int top = (int)Math.Round(1440 / 1.55);
+
+            switch (survivor)
+            {
+                case 1:
+                    left = resolution.Width / 5;
+                    break;
+                case 2:
+                    left = (int)Math.Round(resolution.Width / 1.925);
+                    break;
+                case 3:
+                    left = (int)Math.Round(4.2 * resolution.Width / 5);
+                    break;
+            }
+
+            Bitmap bmp = new Bitmap(375, 35);
+            Graphics g = Graphics.FromImage(bmp);
+            g.CopyFromScreen(left, top, 0, 0, new Size(375, 35), CopyPixelOperation.SourceCopy);
+            bmp.Save(@Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\SoD2Screenshot.jpg");
+
+            Reroll(@path);
+            bmp.Dispose();
+        }
+
+        private void Reroll(string BO3img)
+        {
+            using (var objOcr = OcrApi.Create())
+            {
+                objOcr.Init(Patagames.Ocr.Enums.Languages.English);
+                string plainText = objOcr.GetTextFromImage(@BO3img);
+                string formattedText = Regex.Replace(plainText, @"\s+", "").ToUpper();
+
+                if (survivor == 3 && formattedText.ToUpper().Contains(active[survivor - 1]))
+                {
+                    Stop();
+                }
+                else if (formattedText.ToUpper().Contains(active[survivor - 1]))
+                {
+                    survivor += 1;
+                    sim.Keyboard.KeyPress(VirtualKeyCode.RIGHT);
+                    EnableTimer();
+                }
+                else
+                {
+                    sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+                    EnableTimer();
+                }
+            }
+        }
+
+        private void EnableTimer()
+        {
+            System.Threading.Thread.Sleep(250);
+
+            if (reroll)
+            {
+                rerollTimer.Enabled = true;
+            }
+        }
+
+
+        private void Stop()
+        {
+            survivor = 1;
+            rerollTimer.Interval = wait;
+            reroll = false;
+            rerollTimer.Enabled = false;
+        }
+
+        private void cbSurvivor1_SelectedIndexChanged(object sender, EventArgs e) { active[0] = Regex.Replace(cbSurvivor1.GetItemText(cbSurvivor1.SelectedItem).ToUpper(), @"\s+", ""); }
+        private void cbSurvivor2_SelectedIndexChanged(object sender, EventArgs e) { active[1] = Regex.Replace(cbSurvivor2.GetItemText(cbSurvivor2.SelectedItem).ToUpper(), @"\s+", ""); }
+        private void cbSurvivor3_SelectedIndexChanged(object sender, EventArgs e) { active[2] = Regex.Replace(cbSurvivor3.GetItemText(cbSurvivor3.SelectedItem).ToUpper(), @"\s+", ""); }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            reroll = true;
+            rerollTimer.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Stop();
+        }
+
+        private void nudWait_ValueChanged(object sender, EventArgs e) { wait = Convert.ToInt16(nudWait.Value * 100); }
+
+        private void cbResolution_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] dimensions = cbResolution.GetItemText(cbResolution.SelectedItem).Split('x');
+            resolution.Width = Convert.ToInt16(dimensions[0]);
+            resolution.Height = Convert.ToInt16(dimensions[1]);
+        }
+    }
+}
